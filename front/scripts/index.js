78dc4fdfae4985e764e19d13a003baa4e3db42e4
Luis Vargas <luisvargas@Luiss-MacBook-Air.local>
2025-03-08 15:14:29 -0500

    Added about and contact pages. Mapped each movie into the main page

--- front/scripts/index.js
+++ front/scripts/index.js
@@ -1,1 +1,19 @@
 console.log(tempData);
+
+const main = document.querySelector("main");
+const cardContainer = document.querySelector("#card-container");
+
+tempData.forEach((movie) => {
+    const card = document.createElement("div");
+    card.classList.add("card");
+    card.innerHTML = `
+        <img src="${movie.poster}" alt="${movie.title} Poster">
+        <h2>${movie.title}</h2>
+        <p>Year: ${movie.year}</p>
+        <p>Director: ${movie.director}</p>
+        <p>Duration: ${movie.duration}</p>
+        <p>Genre: ${movie.genre.join(", ")}</p>
+        <p>Rate: ${movie.rate}</p>
+    `;
+    cardContainer.appendChild(card);
+});
