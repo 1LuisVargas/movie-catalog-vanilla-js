6cea8891c310fe42a9100940c74a8ff6f20df60e
Luis Vargas <simplelogin-newsletter.14a9x@simplelogin.com>
2025-03-23 17:37:16 -0500

    Added jQuery to all pages. Removed tempdata from index.html. Changed index.js from using tempdata to get the data from the server

--- front/scripts/index.js
+++ front/scripts/index.js
@@ -1,19 +1,22 @@
-console.log(tempData);
+$.get('https://students-api.up.railway.app/movies', (data) => {
+    renderCards(data);
+});
 
-const main = document.querySelector("main");
 const cardContainer = document.querySelector("#card-container");
 
-tempData.forEach((movie) => {
-    const card = document.createElement("div");
-    card.classList.add("card");
-    card.innerHTML = `
-        <img src="${movie.poster}" alt="${movie.title} Poster">
-        <a href="."><h2>${movie.title}</h2></a>
-        <p>Year: ${movie.year}</p>
-        <p>Director: ${movie.director}</p>
-        <p>Duration: ${movie.duration}</p>
-        <p>Genre: ${movie.genre.join(", ")}</p>
-        <p>Rate: ${movie.rate}</p>
-    `;
-    cardContainer.appendChild(card);
-});
+const renderCards = (data) => {
+    data.forEach((movie) => {
+        const card = document.createElement("div");
+        card.classList.add("card");
+        card.innerHTML = `
+            <img src="${movie.poster}" alt="${movie.title} Poster">
+            <a href="."><h2>${movie.title}</h2></a>
+            <p>Year: ${movie.year}</p>
+            <p>Director: ${movie.director}</p>
+            <p>Duration: ${movie.duration}</p>
+            <p>Genre: ${movie.genre.join(", ")}</p>
+            <p>Rate: ${movie.rate}</p>
+        `;
+        cardContainer.appendChild(card);
+    });
+}
