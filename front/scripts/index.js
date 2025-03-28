3408760e797a0dc7e3a868cdc95b3ab4106e5e70
Luis Vargas <simplelogin-newsletter.14a9x@simplelogin.com>
2025-03-28 11:36:23 -0500

    Changed from index to bundle.js. Added the webpackage. Minor styling changes to titles

--- front/scripts/index.js
+++ front/scripts/index.js
@@ -1,24 +1,5 @@
+const renderCards = require("./renderCards");
+
 $.get('https://students-api.up.railway.app/movies', (data) => {
     renderCards(data);
 });
-
-const cardContainer = document.querySelector("#card-container");
-
-const renderCards = (data) => {
-    data.forEach((movie) => {
-        const card = document.createElement("div");
-        card.classList.add("card", "mb-3", "col-sm-6");
-        card.innerHTML = `
-            <img src="${movie.poster}" class="card-img-top" alt="${movie.title} Poster">
-            <div class="card-body">
-                <a href="#"><h5 class="card-title">${movie.title}</h5></a>
-                <p class="card-text">Year: ${movie.year}<br>
-                Director: ${movie.director}<br>
-                Duration: ${movie.duration}<br>
-                Genre: ${movie.genre.join(", ")}<br>
-                Rate: ${movie.rate}</p>
-            </div>
-        `;
-        cardContainer.appendChild(card);
-    });
-}
